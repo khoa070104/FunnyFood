22fd0c46f86b334a0ded9e44df557e04633b3801
khoa070104 <khoablack71@gmail.com>
2024-12-18 13:35:37 +0700

    init

---
+++ src/main/java/com/khoana/funnyfood/FunnyfoodApplication.java
@@ -0,0 +1,13 @@
+package com.khoana.funnyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khoana/funnyfood/FunnyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoana.funnyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
